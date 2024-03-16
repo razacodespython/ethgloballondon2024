09a3c4faadd325b56627b6a7b5c5d1b4b7d6410f
razacodespython <srh.zaidi@outlook.com>
2024-03-16 20:35:02 +0000

    phaser js

--- diginouns/pages/index.js
+++ diginouns/pages/index.js
@@ -47,10 +47,20 @@ export default function Home() {
       setMsg("Install MetaMask");
     }
   };
+  const setup = async () => {
+    await Promise.all([
+      import("@noir-lang/noirc_abi").then(module => 
+        module.default(new URL("@noir-lang/noirc_abi/web/noirc_abi_wasm_bg.wasm", import.meta.url).toString())
+      ),
+      import("@noir-lang/acvm_js").then(module => 
+        module.default(new URL("@noir-lang/acvm_js/web/acvm_js_bg.wasm", import.meta.url).toString())
+      )
+    ]);
+  }
 
   const sendProof = async (inputt) => {
     console.log("generating proof");
-
+    await setup();
     const provider = new ethers.providers.Web3Provider(window.ethereum, "any");
     await provider.send("eth_requestAccounts", []);
     const signer = provider.getSigner();
